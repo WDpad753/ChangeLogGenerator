9c6a9120d16ae09c0957aae2b900cd22dcfb3a9e
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-06-13 13:28:19 +0100

    Test2

--- ChangeLogConsoleUnitTests/UnitTest1.cs
+++ ChangeLogConsoleUnitTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using BaseLogger;
 using Common.Abstractions;
 
 namespace ChangeLogConsoleUnitTests
@@ -9,12 +10,20 @@ namespace ChangeLogConsoleUnitTests
         [SetUp]
         public void Setup()
         {
+            logwriter = new LogWriter(@"Config\AppTest.config","");
         }
 
         [Test]
         public void Test1()
         {
-
+            if (logwriter == null)
+            {
+                Assert.Fail("LogWriter is not initialized.");
+            }
+            else
+            {
+                Assert.Pass();
+            }
         }
     }
 }
