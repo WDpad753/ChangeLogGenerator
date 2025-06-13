77ee8a674859c746abe2c7f771f0c9858ce6a331
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-06-13 11:54:59 +0100

    fixed minor bugs in code and project.

--- ChangeLogConsoleUnitTests/UnitTest1.cs
+++ ChangeLogConsoleUnitTests/UnitTest1.cs
@@ -1,10 +1,16 @@
+using BaseLogger;
+using Common.Abstractions;
+
 namespace ChangeLogConsoleUnitTests
 {
     public class Tests
     {
+        private ILogWriter logwriter;
+
         [SetUp]
         public void Setup()
         {
+            logwriter = new LogWriter(@"Config\AppTest.config","");
         }
 
         [Test]
