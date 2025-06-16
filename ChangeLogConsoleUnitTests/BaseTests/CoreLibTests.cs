6aa97d1ba41084b767c19f8fe8ec3c80f82d30a7
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-06-16 11:59:05 +0100

    modified the base class and all tests passed for the base class (for now).

--- ChangeLogConsoleUnitTests/BaseTests/CoreLibTests.cs
+++ ChangeLogConsoleUnitTests/BaseTests/CoreLibTests.cs
@@ -11,6 +11,9 @@ namespace ChangeLogConsoleUnitTests.BaseTests
         private ConfigHandler configReader;
         private string logpath;
         private static string LaunchJsonConfigFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}Config\launchsettings.json";
+        private static string envConfigFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}Config\EnvFileTest.env";
+        private static string xmlConfigFilePath1 = @$"{AppDomain.CurrentDomain.BaseDirectory}Config\XMLFileTest1.xml";
+        private static string xmlConfigFilePath2 = @$"{AppDomain.CurrentDomain.BaseDirectory}Config\XMLFileTest2.xml";
 
         [SetUp]
         public void Setup()
@@ -123,6 +126,57 @@ namespace ChangeLogConsoleUnitTests.BaseTests
             string val = "Hello_Unit_Test";
 
             string? res = configReader.EnvRead("Test", EnvAccessMode.File, LaunchJsonConfigFilePath, "environmentVariables");
+
+            if (res != null)
+            {
+                Assert.That(val == res, "Value is not equal after modification");
+            }
+            else
+            {
+                Assert.Fail("Unable to Obtain a Value from Enviroment Variables");
+            }
+        }
+        
+        [Test]
+        public void envConfigEnvReadTest()
+        {
+            string val = "Hello_Unit_Test";
+
+            string? res = configReader.EnvRead("Test", EnvAccessMode.File, envConfigFilePath);
+
+            if (res != null)
+            {
+                Assert.That(val == res, "Value is not equal after modification");
+            }
+            else
+            {
+                Assert.Fail("Unable to Obtain a Value from Enviroment Variables");
+            }
+        }
+        
+        [Test]
+        public void XmlConfigEnvReadTest1()
+        {
+            string val = "Hello_Unit_Test";
+
+            string? res = configReader.EnvRead("Test", EnvAccessMode.File, xmlConfigFilePath1, "EnvironmentVariables");
+
+            if (res != null)
+            {
+                Assert.That(val == res, "Value is not equal after modification");
+            }
+            else
+            {
+                Assert.Fail("Unable to Obtain a Value from Enviroment Variables");
+            }
+        }
+        
+        [Test]
+        public void XmlConfigEnvReadTest2()
+        {
+            string val = "Hello_Unit_Test";
+
+            string? res = configReader.EnvRead("Test", EnvAccessMode.File, xmlConfigFilePath2, "EnvironmentVariables");
 
             if (res != null)
             {
