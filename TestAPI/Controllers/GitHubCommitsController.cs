d3819e69873ec204a0a391ca87a6216e30b2424d
WDpad753 <WDpad753@Admin>
2025-06-17 23:50:44 +0100

    Testing the workflow.

---
+++ TestAPI/Controllers/GitHubCommitsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using TestAPI.Model;
+
+namespace TestAPI.Controllers
+{
+    [ApiController]
+    [Route("/repos/{owner}/{repo}/commits")]
+    public class GitHubCommitsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCommits(
+            string owner,
+            string repo)
+        {
+
+            string fileDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string fullpath = Path.Combine(fileDirectory, "Data");
+
+            var commitsJson = System.IO.File.ReadAllText($"{fullpath}\\GithubCommits.json");
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var response = JsonSerializer.Deserialize<List<GitHubCommit>>(commitsJson, options);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ TestAPI/Model/AzureCommit.cs
@@ -0,0 +1,34 @@
+﻿namespace TestAPI.Model
+{
+    public sealed class AzureCommit
+    {
+        public int Count { get; set; }
+        public List<GitCommit> Value { get; set; } = [];
+    }
+
+    public sealed class GitCommit
+    {
+        public string CommitId { get; set; }
+        public GitUser Author { get; set; }
+        public GitUser Committer { get; set; }
+        public string Comment { get; set; }
+        public bool CommentTruncated { get; set; }
+        public ChangeCounts ChangeCounts { get; set; }
+        public string Url { get; set; }
+        public string RemoteUrl { get; set; }
+    }
+
+    public sealed class GitUser
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public sealed class ChangeCounts
+    {
+        public int Add { get; set; }
+        public int Edit { get; set; }
+        public int Delete { get; set; }
+    }
+}
