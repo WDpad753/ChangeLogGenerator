7f434ffd380814b0af39a1dc6405d463b910e716
Mohamed Mohamed Ahmed <66917039+WDpad753@users.noreply.github.com>
2025-07-22 21:22:49 +0100

    Update Patch-ProjectReferences.ps1

--- tools/Patch-ProjectReferences.ps1
+++ tools/Patch-ProjectReferences.ps1
@@ -2,17 +2,19 @@
 $pattern2 = '..\..\BaseClassLibrary\Common.Abstraction\Common.Abstraction.csproj'
 $pattern3 = '..\..\BaseClassLibrary\BaseLogger\BaseLogger.csproj'
 $pattern4 = '..\..\TestAPI\TestAPI.csproj'
+$pattern5 = '..\..\..\UIBaseClass\UIBaseClass\UIBaseClass.csproj'
 
 $replace1 = '..\BaseClassLibrary\BaseClass\BaseClass.csproj'
 $replace2 = '..\BaseClassLibrary\Common.Abstraction\Common.Abstraction.csproj'
 $replace3 = '..\BaseClassLibrary\BaseLogger\BaseLogger.csproj'
 $replace4 = '..\TestAPI\TestAPI.csproj'
+$replace5 = '..\..\UIBaseClass\UIBaseClass\UIBaseClass.csproj'
 
 $csprojFiles = Get-ChildItem -Recurse -Filter *.csproj
 
 foreach ($file in $csprojFiles) {
     $content = Get-Content $file.FullName -Raw
-    $patched = $content.Replace($pattern1, $replace1).Replace($pattern2, $replace2).Replace($pattern3, $replace3).Replace($pattern4, $replace4)
+    $patched = $content.Replace($pattern1, $replace1).Replace($pattern2, $replace2).Replace($pattern3, $replace3).Replace($pattern4, $replace4).Replace($pattern5, $replace5)
 
     if ($patched -ne $content) {
         Write-Host "Patching: $($file.FullName)"
