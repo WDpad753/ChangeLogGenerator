3804ead2f8c14a76737dfaf2ad94c9d2b49c5f5f
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-10-03 16:35:11 +0100

    Requires Some work and then some testing.

---
+++ ChangeLogConsole/Worker.cs
@@ -0,0 +1,25 @@
+﻿using BaseConsole = BaseClass.ConsoleAppBase.ConsoleBase;
+using BaseClass.ConsoleAppBase.BaseWorker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeLogConsole
+{
+    public class Worker : ConsoleWorkerBase
+    {
+        private readonly BaseConsole _consoleBase;
+
+        public Worker(BaseConsole console)
+        {
+            _consoleBase = console;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
